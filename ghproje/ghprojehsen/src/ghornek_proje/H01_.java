061c2b683b218665adb6a58f804cdac2da702e53
Hasan ŞEN <120592711+hasansennn@users.noreply.github.com>
2022-12-21 21:28:06 +0300

    Add files via upload

---
+++ ghproje/ghprojehsen/src/ghornek_proje/H01_.java
@@ -0,0 +1,9 @@
+package ghornek_proje;
+
+public class H01_ {
+    public static void main(String[] args) {
+
+        String zeynep= "Bugun github dunyasina girdik";
+        System.out.println(zeynep);
+    }
+}
---
+++ ghproje/ghprojehsen/src/ghornek_proje/H02_.java
@@ -0,0 +1,7 @@
+package ghornek_proje;
+
+public class H02_ {
+    public static void main(String[] args) {
+         String omerYigit="Bugun github dunyasina girdik";
+    }
+}
